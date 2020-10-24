aea1f2cdaa998874ac9738407bf5ca27c9cb604d
Epicus <16artem1999@mail.ru>
2020-10-24 14:54:57 +0300

    move onCreateView to BaseFragment.kt

--- app/src/main/java/com/example/superpuperchattbd/messenger/presentation/MessengerFragment.kt
+++ app/src/main/java/com/example/superpuperchattbd/messenger/presentation/MessengerFragment.kt
@@ -1,33 +1,21 @@
 package com.example.superpuperchattbd.messenger.presentation
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.superpuperchattbd.R
 import com.example.superpuperchattbd.common.base.presentation.BaseFragment
 
 class MessengerFragment : BaseFragment() {
 
+    override val layoutId: Int = R.layout.messenger_fragment
+
     private lateinit var viewModel: MessengerViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.messenger_fragment, container, false)
-    }
-
     override fun inject() {
-        TODO("Not yet implemented")
     }
 
     override fun initClickListeners() {
-        TODO("Not yet implemented")
     }
 
     override fun setupViews() {
-        TODO("Not yet implemented")
     }
 
 }
