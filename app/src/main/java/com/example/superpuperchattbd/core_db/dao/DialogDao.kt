e22ebdfa445cbbbd19cc1694b6460a02349109b7
Epicus <16artem1999@mail.ru>
2020-10-27 14:38:30 +0300

    add order by time for dialogs

--- app/src/main/java/com/example/superpuperchattbd/core_db/dao/DialogDao.kt
+++ app/src/main/java/com/example/superpuperchattbd/core_db/dao/DialogDao.kt
@@ -12,7 +12,7 @@ import io.reactivex.Completable
 @Dao
 interface DialogDao {
 
-    @Query("SELECT * FROM ${Table.TABLE_DIALOG}")
+    @Query("SELECT * FROM ${Table.TABLE_DIALOG} ORDER BY lastMessageTime ASC")
     fun getDialogs(): DataSource.Factory<Int, DialogEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
