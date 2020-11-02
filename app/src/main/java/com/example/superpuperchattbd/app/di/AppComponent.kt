e5f168d1522a58929639881ce3a12a8aeaf3085f
ArtorNado <16artem1999@mail.ru>
2020-11-02 12:08:03 +0300

    init router

--- app/src/main/java/com/example/superpuperchattbd/app/di/AppComponent.kt
+++ app/src/main/java/com/example/superpuperchattbd/app/di/AppComponent.kt
@@ -2,7 +2,9 @@ package com.example.superpuperchattbd.app.di
 
 import com.example.superpuperchattbd.app.App
 import com.example.superpuperchattbd.app.di.scope.ApplicationScope
-import com.example.superpuperchattbd.common.di.ViewModelFactoryModule
+import com.example.superpuperchattbd.common.di.navigation.NavigationModule
+import com.example.superpuperchattbd.common.di.navigation.RouterModule
+import com.example.superpuperchattbd.common.di.viewModel.ViewModelFactoryModule
 import com.example.superpuperchattbd.core_db.di.DatabaseModule
 import com.example.superpuperchattbd.messenger.di.MessengerFeatureSubcomponent
 import dagger.BindsInstance
@@ -10,7 +12,9 @@ import dagger.Component
 
 @ApplicationScope
 @Component(
-    modules = [AppModule::class, ViewModelFactoryModule::class, DatabaseModule::class]
+    modules =
+    [AppModule::class, ViewModelFactoryModule::class, DatabaseModule::class,
+        RouterModule::class, NavigationModule::class]
 )
 interface AppComponent {
 
