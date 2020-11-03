f047a0a6421d807200e5ac058934b9924af01d5d
GribanovaNastya <hafizova-i@mail.ru>
2020-11-03 21:06:13 +0300

    Start of work with theme

---
+++ app/src/main/java/com/example/superpuperchattbd/theme/presentation/ThemeFragment.kt
@@ -0,0 +1,25 @@
+package com.example.superpuperchattbd.theme.presentation
+
+import com.example.superpuperchattbd.common.base.BaseFragment
+import com.example.superpuperchattbd.messenger.presentation.MessengerViewModel
+
+class ThemeFragment : BaseFragment<ThemeViewModel>(){
+
+    override val layoutId: Int = com.example.superpuperchattbd.R.layout.theme_fragment
+
+    override fun inject() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initClickListeners() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setupViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun subscribe() {
+        TODO("Not yet implemented")
+    }
+}
