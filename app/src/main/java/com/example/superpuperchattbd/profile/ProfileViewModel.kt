821abc2bbb5c144436576fa9424d5c8225398699
DinaShafeeva <49372442+DinaShafeeva@users.noreply.github.com>
2020-11-08 17:45:49 +0300

    Added data binding and dagger in profile fragment

--- app/src/main/java/com/example/superpuperchattbd/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/superpuperchattbd/profile/ProfileViewModel.kt
@@ -5,18 +5,24 @@ import androidx.lifecycle.MutableLiveData
 import com.example.superpuperchattbd.common.base.BaseViewModel
 import com.example.superpuperchattbd.core_db.model.ProfileEntity
 import com.example.superpuperchattbd.messenger.router.MessengerRouter
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class ProfileViewModel(
-    private val interactor: ProfileInteractor,
-    private val router: MessengerRouter
+    private val interactor: ProfileInteractor
+   // private val router: MessengerRouter
 ) : BaseViewModel() {
 
     private val _data = MutableLiveData<ProfileEntity>()
     var data: LiveData<ProfileEntity> = _data
 
-//    init {
-//        disposables.add(
-//            interactor.getData()
-//        )
-//    }
+    init {
+        disposables.add(
+            interactor.getData().observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe({
+                _data.value = it
+            }, {
+                it.printStackTrace()
+            })
+        )
+    }
 }
