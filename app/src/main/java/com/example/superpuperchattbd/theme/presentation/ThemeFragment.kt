c7503fd254b6931895cb9058262cf3d8936ac815
GribanovaNastya <hafizova-i@mail.ru>
2020-11-08 19:55:56 +0300

    Add night theme

--- app/src/main/java/com/example/superpuperchattbd/theme/presentation/ThemeFragment.kt
+++ app/src/main/java/com/example/superpuperchattbd/theme/presentation/ThemeFragment.kt
@@ -1,25 +1,51 @@
 package com.example.superpuperchattbd.theme.presentation
 
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.superpuperchattbd.R
 import com.example.superpuperchattbd.common.base.BaseFragment
-import com.example.superpuperchattbd.messenger.presentation.MessengerViewModel
+import kotlinx.android.synthetic.main.theme_fragment.*
 
 class ThemeFragment : BaseFragment<ThemeViewModel>(){
 
     override val layoutId: Int = com.example.superpuperchattbd.R.layout.theme_fragment
+    lateinit var animation: Animation
+    private val KEY_THEME = "Theme"
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        val themeSharedPreferences = this.activity?.getPreferences(Context.MODE_PRIVATE)
+        themeSharedPreferences?.getInt(KEY_THEME, 1)?.let { AppCompatDelegate.setDefaultNightMode(it) }
+        return inflater.inflate(R.layout.theme_fragment, container, false)
+    }
 
     override fun inject() {
-        TODO("Not yet implemented")
     }
 
     override fun initClickListeners() {
-        TODO("Not yet implemented")
+        val themeSharedPreferences = this.activity?.getPreferences(Context.MODE_PRIVATE)
+
+        btn_theme.setOnClickListener{
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                themeSharedPreferences?.edit()?.putInt(KEY_THEME, AppCompatDelegate.MODE_NIGHT_NO)?.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                themeSharedPreferences?.edit()?.putInt(KEY_THEME, AppCompatDelegate.MODE_NIGHT_YES)?.apply()
+            }
+        }
     }
 
     override fun setupViews() {
-        TODO("Not yet implemented")
     }
 
     override fun subscribe() {
-        TODO("Not yet implemented")
     }
 }
